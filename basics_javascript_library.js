1b46eb7fd2efdba5c49666a82dc499d07f9bdac2
JCarr Dev <carrjr1992@gmail.com>
2019-05-10 09:35:26 -0700

    General Purpose Notes added

--- basics_javascript_library.js
+++ basics_javascript_library.js
@@ -1,3 +1,20 @@
+/*--------------------------------------
+
+This is a JavaScript library reference from FreeCodeCamp.
+I created this file to track my progress through the 
+FreeCodeCampCamp "JavaScript Algorithms and Data Structures
+Certification" All Code examples below are from FreeCodeCamp
+to an extent. Comments show you what each Code snipit should
+produce. This file is currently in progress. At the end of my 
+progression I may divide this file into segments to showcase
+each part of the JavaScript track on FreeCodeCamp. Shoutout
+to FreeCodeCamp on the awesome curriculum they provide and
+tools to jump start you on your journey to becoming a 
+developer. Never stop learning and only use this tool
+as reference for the freeCodeCamp Curriculum as you progress.
+ 
+------First Track Basic JavaScript--------
+----------------------------------------*/
 // inline comment
 
 /* Multi-line
