0ca326b52152ec8c4ec942fdd964265e2d0e8b27
JCarr Dev <carrjr1992@gmail.com>
2019-05-28 14:45:48 -0700

    Update Regular Expressions FreeCodeCamp

--- RegularExpression.js
+++ RegularExpression.js
@@ -108,7 +108,7 @@ let result = chewieQuote.match(chewieRegex);
 //Regular Expressions:Find Character with Lazy Matching
 
 let text = "<h1>Winter is coming</h1>";
-let myRegex = /<.*?>/; // ? is lazy matching will smallest sub-string possible to fit the pattern
+let myRegex = /<.*?>/; // ? is lazy matching, finds the smallest sub-string possible to fit the pattern
 let result = text.match(myRegex);
 //Result <h1> (Note if your testing in chrome on a webpage text is often already declared may through an error)
 
@@ -141,10 +141,79 @@ let result = lastRegex.test(caboose);
 //Into to Shorthand Character classes
 
 let quoteSample = "The five boxing wizards jump quickly.";
-let alphabetRegexV2 = /\w/g; // shorthand character class for [A-Za-z0-9_]
+let alphabetRegexV2 = /\w/g; // \w shorthand character class for [A-Za-z0-9_] ---Alphanumerics string Pattern
 let result = quoteSample.match(alphabetRegexV2).length;
 //Result 31
 
 //Regular Expressions:Match Everything But Letters and Numbers
 
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // \W shorthand character class [^A-Za-z0-9_] ---Alphanumerics string Pattern
+let result = quoteSample.match(nonAlphabetRegex).length;
 
+//Regular Expressions:Match All Numbers
+
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\d/g; // \d shorthand character class [0-9] ---Digit Sting Pattern
+let result = numString.match(numRegex).length;
+
+//Regular Expressions:Match All Non-Numbers
+
+let numString = "Your sandwich will be $5.00";
+let noNumRegex = /\D/g; // \D shorthand character class [^0-9] ---Digit String Pattern
+let result = numString.match(noNumRegex).length;
+
+//Regular Expressions:Restrict Possible Usernames
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]\D/i; // Checks for any lower or uppercase letter with only letters in the beginning and isnt a number (This Passed the freeCodeCamp Test)
+// let userCheck = /^[a-z]{2,}\d*$/i; --- This is a better solution from the FreeCodeCamp Hint (Not sure why mine passed but decided to go back and understand)
+// ****/\d*$/; --Checks for only numbers at the end there can only be zero or more of them
+// ****/d*$/i -- Flags all upper and lowercase characters
+// ****/^[a-z]{2,}\d*$/i -- Finally Uses letters a-z that are at least 2 characters long and only two letter usernames can be alphabet letter characters
+//FreeCodeCamp Doesnt teach the {} in the regular expression challenges at this point only the *, +, ? 
+//MDN says -- These are what are known as quantifiers you can read more about them on MDN
+let selfCheck = console.log(username.match(userCheck)); // Used for self check to see value 
+let result = userCheck.test(username);
+
+//Regular Expressions:Match Whitespace
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // shorthand for whitespace or character class [ \r\t\f\n\v]
+let result = sample.match(countWhiteSpace);
+
+//Regular Expressions:Match Non-Whitespace Characters
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // shorthand for whitespace or character class [^ \r\t\f\n\v]
+let result = sample.match(countNonWhiteSpace);
+
+//Regular Expressions:Specify Upper and Lower Number of Matches
+
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6} no/; // quantity specifiers use {} they create a range for a pattern in this case find h between 3-6 times 
+let result = ohRegex.test(ohStr);
+
+//Regular Expressions:Specify Only the Lower Number of Matches
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // quantity specifiers use {} this pattern doesnt use a upper limit for the pattern so it finds the lowest {4,} (z's) in this case but also doesnt have a limit so can continue to infinity
+let result = haRegex.test(haStr);
+
+//Regular Expressions:Specify Exact Number of Matches
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // quantity specifiers use {} this pattern doesnt use a lower or upper just a specific value for the pattern in this case {4} so it only finds the value m in Timber 4 times.
+let result = timRegex.test(timStr);
+
+//Regular Expressions:Check for All or None
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // ? in this case is being used as an assertion(MDN) --Specify the possible existence of an element (The previous element is optional)
+let result = favRegex.test(favWord); // ---Look into Assertions a little more on MDN it will help clarify a little more
+
+//Regular Expressions:Positive and Negative Lookahead
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{5,})(?=\D*\d{2})/; // Lookahead assertion Matched x only if x is followed by y.In this case Any characters 5 or greater and have two consecutive digits
+let result = pwRegex.test(sampleWord);
